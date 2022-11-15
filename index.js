bea86279da4b5c16a95a10bf63a9c3906ce0871d
Govin_R <rgovidaraj6092@gmail.com>
2022-11-15 21:40:02 +0530

    rotate_achived_timeUpdateNeed

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 
 const countdown = document.getElementById('countdown');
+let intervalId;
+let value=0;
 
-//creating no of divs
+function flipIt(){
 
-let elements=['Days','Hours','Minutes','Seconds'];
-
-for(let i = 0; i<elements.length; i++ ){
-	let child1 = document.createElement('div');
-	child1.setAttribute('id','timer');
-
-	let oneHalf = document.createElement('div');
-	oneHalf.setAttribute('id','oneHalf');
-	oneHalf.innerText="22";
-	let secondHalf = document.createElement('div');
-	secondHalf.setAttribute('id',"secondHalf");
-	secondHalf.innerText="22";
-	let backup = document.createElement('div');
-	backup.setAttribute('id','backup');
-	//child1.innerText="Hello world";
-	
-	backup.appendChild(oneHalf);
-	backup.appendChild(secondHalf);
-	child1.appendChild(backup);
-
-
-	countdown.appendChild(child1);
-	//console.log(elements[i]);
+	intervalId = setInterval(()=>{
+		spans.forEach((a)=>{
+			document.getElementById(a).innerText=String(value);
+			document.getElementById(a).style.transform="rotatex(50deg)";		
+		})
+		value+=1;
+	},1000)
 }
+
+flipIt();
